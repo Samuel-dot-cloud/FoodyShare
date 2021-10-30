9adf23f09cecfd8c6e711ade9ce7241d06e36d20
Samuel-dot-cloud <successam336@gmail.com>
2021-10-30 17:47:21 +0300

    Created list card and successfully displayed favorite lists

--- lib/widgets/recipe/create_list_form.dart
+++ lib/widgets/recipe/create_list_form.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:food_share/services/firebase_operations.dart';
+import 'package:food_share/utils/loading_animation.dart';
 import 'package:food_share/utils/pallete.dart';
 import 'package:food_share/widgets/rounded_button.dart';
+import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class CreateListForm extends StatefulWidget {
   const CreateListForm({Key? key}) : super(key: key);
@@ -10,66 +15,107 @@ class CreateListForm extends StatefulWidget {
 }
 
 class _CreateListFormState extends State<CreateListForm> {
-  late final String _name = '';
-  late final String _description = '';
+  late String _name;
+  late String _description;
+  String listID = const Uuid().v4();
+  final Timestamp timestamp = Timestamp.now();
+  bool _isCreating = false;
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(24.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            _buildFormField('List name', 'List name is required', _name),
-            const SizedBox(
-              height: 30.0,
-            ),
-            _buildFormField('Description (optional)', '', _description),
-            const SizedBox(
-              height: 30.0,
-            ),
-            RoundedButton(
-              onPressed: () {
-                if (!_formKey.currentState!.validate()) {
-                  return;
-                }
-                _formKey.currentState!.save();
-              },
-              buttonName: 'Create',
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+    return !_isCreating
+        ? Container(
+            margin: const EdgeInsets.all(20.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  // _buildFormField('List name', 'List name is required', _name),
+                  TextFormField(
+                    cursorColor: kBlue,
+                    decoration: const InputDecoration(
+                      labelText: 'List name',
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: kBlue)),
+                      labelStyle: TextStyle(
+                        color: kBlue,
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'List name is required';
+                      }
+                    },
+                    onSaved: (value) {
+                      _name = value!;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  // _buildFormField('Description (optional)', '', _description),
+                  TextFormField(
+                    cursorColor: kBlue,
+                    decoration: const InputDecoration(
+                      labelText: 'Description (optional)',
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: kBlue)),
+                      labelStyle: TextStyle(
+                        color: kBlue,
+                      ),
+                    ),
+                    onSaved: (value) {
+                      _description = value!;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  RoundedButton(
+                    onPressed: () {
+                      if (!_formKey.currentState!.validate()) {
+                        return;
+                      }
+                      _formKey.currentState!.save();
 
-  TextFormField _buildFormField(String text, String errorText, String _data) {
-    return TextFormField(
-      cursorColor: kBlue,
-      decoration: InputDecoration(
-        labelText: text,
-        enabledBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey)),
-        focusedBorder:
-            const UnderlineInputBorder(borderSide: BorderSide(color: kBlue)),
-        labelStyle: const TextStyle(
-          color: kBlue,
-        ),
-      ),
-      validator: (value) {
-        if (value!.isEmpty) {
-          return errorText;
-        }
-      },
-      onSaved: (value) {
-        _data = value!;
-      },
-    );
+                      setState(() {
+                        _isCreating = true;
+                      });
+
+                      Provider.of<FirebaseOperations>(context, listen: false)
+                          .createFavoriteList(
+                        Provider.of<FirebaseOperations>(context, listen: false)
+                            .getUserId,
+                        listID,
+                        {
+                          'name': _name,
+                          'description': _description,
+                          'recipe_count': 0,
+                          'timestamp': timestamp,
+                          'lastEdited': timestamp,
+                        },
+                      ).whenComplete(() {
+                        setState(() {
+                          _isCreating = false;
+                        });
+                        Navigator.pop(context);
+                      });
+                    },
+                    buttonName: 'Create',
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+            ),
+          )
+        : loadingAnimation('Creating list...');
   }
 }
